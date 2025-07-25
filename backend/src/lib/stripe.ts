3d3d1d638ab08513a891af1f778aa3ce7d376de3
Paiman-Rasoli <paimanrasoli789@gmail.com>
2025-07-25 18:12:32 +0430

    backend: create stripe customer after registration

--- backend/src/lib/stripe.ts
+++ backend/src/lib/stripe.ts
@@ -1,3 +1,29 @@
 import Stripe from "stripe";
 
-export const stripeClient = new Stripe(process.env.STRIPE_SECRET_KEY!);
+export class StripeClient {
+  private stripe: Stripe;
+
+  constructor() {
+    this.stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+  }
+
+  async createCustomer(input: { email: string; userId: string }) {
+    try {
+      const { email, userId } = input;
+
+      const customer = await this.stripe.customers.create({
+        email,
+        metadata: {
+          userId: userId,
+        },
+      });
+      return customer.id;
+    } catch (error: any) {
+      console.error(
+        `Error creating stripe customer for user ${input.userId}`,
+        error?.message
+      );
+      return null;
+    }
+  }
+}
